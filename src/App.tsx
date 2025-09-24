cd369495074ff839aa28e861e2e02d507a6bd8eb
Danilo Jesús <danilojesus112001@gmail.com>
2025-09-24 10:29:07 -0300

    chore: adicionando o Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import Rotas from './routes'
 
 import { GlobalStyle } from './styles'
+import { store } from './store'
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Rotas />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Rotas />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
